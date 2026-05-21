c0b347847639979f29db613441197491d29ee931
Lautaro Colella <lautarocolella1@gmail.com>
2026-05-21 08:19:31 -0300

    add scully config again, update sitemap lastmod, remove head and body tags inside components, move bootstrap and fontawesome to root html, add canonical url, fix loader component duplication error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,23 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavbarComponent } from '@@shared/navbar/navbar.component';
-import { IndexComponent } from '@@pages/index/index.component';
-import { WorkComponent } from '@@pages/work/work.component';
 import { AboutComponent } from '@@pages/about/about.component';
-import { ContactComponent } from '@@pages/contact/contact.component';
-import { LoginComponent } from '@@pages/login/login.component';
 import { AdminComponent } from '@@pages/admin/admin.component';
-import { AdminHomeComponent } from '@@pages/admin/views/admin-home/admin-home.component';
-import { AdminWorkComponent } from '@@pages/admin/views/admin-work/admin-work.component';
 import { AdminAboutComponent } from '@@pages/admin/views/admin-about/admin-about.component';
 import { AdminContactComponent } from '@@pages/admin/views/admin-contact/admin-contact.component';
+import { AdminHomeComponent } from '@@pages/admin/views/admin-home/admin-home.component';
 import { AdminUserComponent } from '@@pages/admin/views/admin-user/admin-user.component';
+import { AdminWorkComponent } from '@@pages/admin/views/admin-work/admin-work.component';
+import { ContactComponent } from '@@pages/contact/contact.component';
+import { IndexComponent } from '@@pages/index/index.component';
+import { LoginComponent } from '@@pages/login/login.component';
+import { WorkComponent } from '@@pages/work/work.component';
+import { LoaderModule } from '@@shared/loader/loader.module';
+import { NavbarComponent } from '@@shared/navbar/navbar.component';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 import { HttpErrorInterceptor } from './interceptors/error-and-cache.interceptor';
 import { LoaderInterceptor } from './interceptors/loader.interceptor';
-import { LoaderComponent } from '@@shared/loader/loader.component';
 
 @NgModule({
 	declarations: [
@@ -33,13 +33,13 @@ import { LoaderComponent } from '@@shared/loader/loader.component';
 		AdminWorkComponent,
 		AdminAboutComponent,
 		AdminContactComponent,
-		AdminUserComponent,
-		LoaderComponent
+		AdminUserComponent
 	],
 	imports: [
 		BrowserModule,
 		HttpClientModule,
-		AppRoutingModule
+		AppRoutingModule,
+		LoaderModule
 	],
 	providers: [
 		{
