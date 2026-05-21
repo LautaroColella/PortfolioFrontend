88129f1b312ceb6475bb3bc0e8be820bad8005a9
Lautaro Colella <lautarocolella1@gmail.com>
2026-05-21 08:42:04 -0300

    add chromium browser for puppeteer in scully and github deploy

--- scully.portfolio.config.ts
+++ scully.portfolio.config.ts
@@ -1,10 +1,17 @@
 import { ScullyConfig } from '@scullyio/scully';
+import '@scullyio/scully-plugin-puppeteer';
 
 export const config: ScullyConfig = {
   projectRoot: './src',
   projectName: 'portfolio',
   distFolder: './dist/portfolio',
   outDir: './dist/portfolio',
+
+  puppeteerLaunchOptions: {
+    executablePath: process.env['CHROME_BIN'],
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+  },
+
   defaultPostRenderers: [],
   routes: {},
 };
